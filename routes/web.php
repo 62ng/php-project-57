0e6c0bff8f28c88a5b0eb6c85fd843cdda184dd2
Andrey Frolov <internet.buro@gmail.com>
2022-10-11 11:37:52 +0300

    Add breeze authentication

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    \Log::debug('Test heroku debug message');
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
