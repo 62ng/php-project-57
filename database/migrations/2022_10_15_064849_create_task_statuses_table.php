bb95ba78e2a6129e95aa3a26da755ad16dd71489
Andrey Frolov <internet.buro@gmail.com>
2022-11-03 20:55:08 +0300

    Delete migration

--- database/migrations/2022_10_15_064849_create_task_statuses_table.php
+++ database/migrations/2022_10_15_064849_create_task_statuses_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('task_statuses', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->boolean('status');
             $table->timestamps();
         });
     }
