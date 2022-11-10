e763397b110a5784ec8df525ec938bc28220c41d
Andrey Frolov <internet.buro@gmail.com>
2022-11-10 16:25:49 +0300

    Fix email validation with dns option

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -18,7 +18,7 @@ class RegistrationTest extends TestCase
     {
         $response = $this->post('/register', [
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@google.com',
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
